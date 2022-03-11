c79ec464d083f516b52834dc33fd5d39170bee6f
Joe Morris <joemorris@RazerBlade.localdomain>
2022-03-11 14:50:52 +0000

    Added equation module but not working right now

--- types/zcfd/src/model.js
+++ types/zcfd/src/model.js
@@ -1,5 +1,5 @@
 module.exports = {
-  order: ['timemarcher', 'solverscheme'],
+  order: ['timemarcher', 'solverscheme','cfldata'],
   views: {
     timemarcher: {
       size: -1,
@@ -28,6 +28,19 @@ module.exports = {
       ],
     },   
   },
+  cfldata: {
+    size: -1,
+    attributes: ['cfl'],
+    hooks: [
+      {
+        type: 'copyParameterToViewName',
+        attribute: 'cfl.name',
+      },
+      {
+        type: 'cflToExternal',
+      },
+    ],
+  },  
   definitions: {
     timemarch: {
       parameters: [
@@ -188,6 +201,70 @@ module.exports = {
           }
         },                                             
       ],
-    },    
+    },
+    cfl: {
+      parameters: [
+        {
+          id: 'cflname',
+          label: 'CFL Settings Name',
+          type: 'string',
+          size: 1,
+        },
+        {
+          id: 'maxcfl_all',
+          type: 'double',
+          size: 1,         
+          default: [2.5],
+        },
+        {
+          id: 'maxcfl_transport',
+          type: 'double',
+          size: 1,         
+          default: [1.5],
+        },
+        {
+          id: 'maxcfl_coarse',
+          type: 'double',
+          size: 1,         
+          default: [2.0],
+        },  
+        {
+          id: 'multipolycfl_lower',
+          type: 'double',
+          size: 1,         
+          default: [2.0],
+        },
+        {
+          id: 'multipolycfl_upper',
+          type: 'double',
+          size: 1,         
+          default: [2.0],
+        },
+        {
+          id: 'ramp_factor',
+          type: 'enum',
+          size: 1,
+          default: 'no',
+          domain: {
+            Yes: 'yes',
+            No: 'no',
+          }
+        }, 
+        {
+          id: 'growth',
+          type: 'double',
+          size: 1,
+          show: "type[0] === 'yes'",                   
+          default: [1.05],
+        },
+        {
+          id: 'initial',
+          type: 'double',
+          size: 1,
+          show: "type[0] === 'yes'",                   
+          default: [0.1],
+        },                                                    
+      ],
+    },         
   },
 };
